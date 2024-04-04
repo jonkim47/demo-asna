b5f01fb338fe5d211a5e5ae233c493c6a8492da6
Jonathan Kim <jonkim47@gmail.com>
2024-04-04 18:44:35 -0400

    update for asana

--- constants.js
+++ constants.js
@@ -2,83 +2,71 @@ const LOGGED_IN_KEY = "loggedIn";
 
 const ACTIONS = [
   {
-    title: "Schedule a Telemedicine Appointment",
-    image: "./assets/schedule-telemedicine.jpeg",
-    description:
-      "Book a virtual appointment with healthcare professionals for medical advice and prescriptions.",
+    title: "Open Trial Account",
+    image: "./assets/open-account.jpeg",
+    description: "Easily organize your work. Start free. Access Asana's features. No credit card required.",
     properties: [
-      { name: "Full Name", value: "John Doe" },
-      { name: "Contact Number", value: "555-1234" },
-      { name: "Preferred Date", value: "2023-12-10" },
-      { name: "Preferred Time", value: "15:30" },
-      { name: "Reason for Appointment", value: "General health checkup" },
-    ],
+      { name: "Account Type", value: "Trial" },
+      { name: "Account Tier", value: "Personal" },
+      { name: "Cost", value: "$0" }
+    ]
   },
   {
-    title: "Complete a Digital Health Assessment",
-    image: "./assets/digital-health-assessment.jpeg",
-    description:
-      "Take a comprehensive digital health assessment to receive personalized health recommendations.",
+    title: "Download App",
+    image: "./assets/transfer-funds.jpeg",
+    description: "Asana’s native desktop app removes distractions so you can focus on what's important. Just download according to your OS, login with your email on the web, and you'll be guided to your new favorite app.",
     properties: [
-      { name: "Full Name", value: "Jane Smith" },
-      { name: "Date of Birth", value: "1985-05-20" },
-      { name: "Gender", value: "Female" },
-      { name: "Health Conditions", value: "None" },
-      { name: "Lifestyle Habits", value: "Regular exercise, balanced diet" },
-    ],
+      { name: "OS", value: "MacOS" },
+      { name: "App Version", value: "2.1.2" }
+    ]
   },
   {
-    title: "Place an Order on the Virtual Pharmacy",
-    image: "./assets/virtual-pharmacy.jpeg",
-    description:
-      "Order prescription medications and healthcare products online, with secure and discreet delivery.",
+    title: "View Advanced Pricing",
+    image: "./assets/purchase-crypto.jpeg",
+    description: "For companies that need to manage a portfolio of work and goals across departments.",
     properties: [
-      { name: "Full Name", value: "Alex Johnson" },
-      { name: "Delivery Address", value: "123 Main St, Cityville" },
-      { name: "Prescription Details", value: "Medication X, 30 tablets" },
-      { name: "Additional Comments", value: "Leave package at the front door" },
-    ],
+      { name: "Account Tier", value: "Advanced" },
+      { name: "Cost", value: "$24.99" },
+      { name: "Payment Cycle", value: "Annually" },
+      { name: "Payment Method", value: "Credit Card" }
+    ]
   },
   {
-    title: "Subscribe to the Newsletter",
-    image: "./assets/newsletter-subscription.jpeg",
-    description:
-      "Stay informed by subscribing to our newsletter for the latest healthcare updates and promotions.",
+    title: "Attend Webinar",
+    image: "./assets/manage-investments.jpeg",
+    description: "Meet Asana Intelligence: AI has joined the team",
     properties: [
-      { name: "Email Address", value: "alex@example.com" },
-      { name: "Full Name", value: "Alex Johnson" },
-      { name: "Interests", value: "General Health, Wellness Tips" },
-    ],
+      { name: "Resource Category", value: "Live Webinar" }
+    ]
   },
   {
-    title: "Engage with a Blog Post",
-    image: "./assets/blog-engagement.jpeg",
-    description:
-      "Read, like, and share informative blog posts on various healthcare topics.",
+    title: "Read Blog",
+    image: "./assets/pay-bills.jpeg",
+    description: "Make payments for bills, loans, or recurring expenses through your financial account, often with the option to set up automated payments for convenience.",
     properties: [
-      { name: "Full Name", value: "Samantha Williams" },
-      { name: "Email Address", value: "samantha@example.com" },
-      { name: "Comment", value: "Great article, very informative!" },
-      { name: "Share on Social Media", value: "Yes" },
-    ],
+      { name: "Biller Information", value: "Name of the Company" },
+      { name: "Bill Amount", value: "$100" },
+      { name: "Payment Date", value: "Due Date" },
+      { name: "Payment Method", value: "Online Banking" }
+    ]
   },
   {
-    title: "Complete a Contact Form Submission",
-    image: "./assets/contact-form.jpeg",
-    description:
-      "Reach out to us with inquiries, feedback, or requests through our online contact form.",
+    title: "Request Customer Support",
+    image: "./assets/customer-support.jpeg",
+    description: "Contact customer support to seek assistance with account-related issues, resolve problems, or ask questions regarding products and services.",
     properties: [
-      { name: "Full Name", value: "Robert Brown" },
-      { name: "Email Address", value: "robert@example.com" },
-      { name: "Phone Number", value: "555-5678" },
-      { name: "Message", value: "I have a billing inquiry." },
-    ],
-  },
+      { name: "Customer Name", value: "Your Name" },
+      { name: "Contact Information", value: "Email" },
+      { name: "Issue Description", value: "Transaction Inquiry" },
+      { name: "Priority Level", value: "High" },
+      { name: "Request Timestamp", value: "Date and Time of Request" }
+    ]
+  }
 ];
 
 const HEADER_NAV_CONFIG = [
   { htmlId: "homeLink", pageName: "Home" },
-  { htmlId: "servicesLink", pageName: "Savings" },
-  { htmlId: "solutionsLink", pageName: "Investments" },
-  { htmlId: "contactLink", pageName: "Crypto" },
+  { htmlId: "servicesLink", pageName: "Features" },
+  { htmlId: "solutionsLink", pageName: "Solutions" },
+  { htmlId: "contactLink", pageName: "Resources" },
 ];
